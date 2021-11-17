248bb0cf9316bc37553a3d82d8082b8833ec1c93
Afroza Akter <afroza@asl.aero>
2021-11-17 17:40:20 +0600

    Header Design and Toggle Sidebar updated

--- src/component/Header/Home.js
+++ src/component/Header/Home.js
@@ -1,10 +1,82 @@
 import React from 'react';
+import Accordion from 'react-bootstrap/Accordion';
+import { FaUsers } from "react-icons/fa";
+
 
 const Apps = () => {
     return (
-        <div>
-            <h2>Home</h2>
+        <section id="Accordions">
+           <div className="Accordions__left">
+           <Accordion defaultActiveKey="0">
+  <Accordion.Item eventKey="0">
+    <Accordion.Header>Most tracked flights <div className="Accordion-Header--live">Live</div><p className="Accordion-Header--question">?</p></Accordion.Header>
+    <Accordion.Body>
+      <div className="Accordion-body__content">
+      <div className="Accordion-body__content--inside">
+         
+                 <div className="Accordion-body__content--inside__number">1</div>
+                 <span className="Accordion-body__content--inside__content">FR334/RYR7BJ</span><br/>
+                 <small className="Accordion-body__content--inside__content-small">Pafos(PFO)-Tallinn(TILL)</small>
+                 <div className="Accordion-body__content--inside__icon">540 <FaUsers className="Accordion-body__content--inside__icon--users"/></div>
+
         </div>
+
+        <div className="Accordion-body__content--inside">
+         
+                 <div className="Accordion-body__content--inside__number">2</div>
+                 <span className="Accordion-body__content--inside__content">FR334/RYR7BJ</span><br/>
+                 <small className="Accordion-body__content--inside__content-small">Pafos(PFO)-Tallinn(TILL)</small>
+                 <div className="Accordion-body__content--inside__icon">540 <FaUsers className="Accordion-body__content--inside__icon--users"/></div>
+
+        </div>
+
+        <div className="Accordion-body__content--inside">
+         
+                 <div className="Accordion-body__content--inside__number">3</div>
+                 <span className="Accordion-body__content--inside__content">FR334/RYR7BJ</span><br/>
+                 <small className="Accordion-body__content--inside__content-small">Pafos(PFO)-Tallinn(TILL)</small>
+                 <div className="Accordion-body__content--inside__icon">540 <FaUsers className="Accordion-body__content--inside__icon--users"/></div>
+
+        </div>
+      </div>
+    </Accordion.Body>
+  </Accordion.Item>
+  <Accordion.Item eventKey="1">
+  <Accordion.Header>Last Clicked flights <p className="Accordion-Header--question">?</p></Accordion.Header>
+    <Accordion.Body>
+    <div className="Accordion-body__content">
+      <div className="Accordion-body__content--inside">
+         
+                 <div className="Accordion-body__content--inside__number">1</div>
+                 <span className="Accordion-body__content--inside__content">FR334/RYR7BJ</span><br/>
+                 <small className="Accordion-body__content--inside__content-small">Pafos(PFO)-Tallinn(TILL)</small>
+                 <div className="Accordion-body__content--inside__icon">540 <FaUsers className="Accordion-body__content--inside__icon--users"/></div>
+
+        </div>
+
+        <div className="Accordion-body__content--inside">
+         
+                 <div className="Accordion-body__content--inside__number">2</div>
+                 <span className="Accordion-body__content--inside__content">FR334/RYR7BJ</span><br/>
+                 <small className="Accordion-body__content--inside__content-small">Pafos(PFO)-Tallinn(TILL)</small>
+                 <div className="Accordion-body__content--inside__icon">540 <FaUsers className="Accordion-body__content--inside__icon--users"/></div>
+
+        </div>
+
+        <div className="Accordion-body__content--inside">
+         
+                 <div className="Accordion-body__content--inside__number">3</div>
+                 <span className="Accordion-body__content--inside__content">FR334/RYR7BJ</span><br/>
+                 <small className="Accordion-body__content--inside__content-small">Pafos(PFO)-Tallinn(TILL)</small>
+                 <div className="Accordion-body__content--inside__icon">540 <FaUsers className="Accordion-body__content--inside__icon--users"/></div>
+
+        </div>
+      </div>
+    </Accordion.Body>
+  </Accordion.Item>
+</Accordion>
+           </div>
+        </section>
     )
 }
 
--- src/component/Header/Menu.js
+++ src/component/Header/Menu.js
@@ -1,30 +1,40 @@
 import React from 'react';
-import { Navbar, Container, Nav, NavDropdown} from 'react-bootstrap';
+import { Navbar, Container, Nav, NavDropdown, Dropdown} from 'react-bootstrap';
 import { NavLink } from "react-router-dom";
+import { FaUserCircle } from "react-icons/fa";
 
 const Menu = () => {
     return (
         <div className="main">
     <Navbar expand="lg">
-  <Container>
+  <Container fluid>
     <Navbar.Brand href="#home" className="logo"><img src="/Images/logo-v2.png"/></Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Toggle className="toggle__color" aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav>
         <NavLink className="link1" activeClassName="active_class" to="/">App</NavLink>
         <NavLink className="link1" to="/">Data</NavLink>
-        <NavDropdown title="Recovery" id="basic-nav-dropdown">
+        <NavDropdown className="drop" activeClassName="active_class" title="Recovery" id="basic-nav-dropdown">
           <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
           <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
           <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
           
         </NavDropdown>
-        <NavLink className="link1" activeClassName="active_class" to="/viewaircraft">View Aircraft</NavLink>
+        <NavLink className="link2" activeClassName="active_class" to="/viewaircraft">View Aircraft</NavLink>
         <NavLink className="link1" to="/">Social</NavLink>
         <NavLink className="link1" to="/">About</NavLink> 
       </Nav>
     </Navbar.Collapse>
-  </Container>0
+    <Dropdown>
+  <Dropdown.Toggle id="dropdown-basic">
+  <FaUserCircle className="dropdown-basic__icon"/>Log In
+  </Dropdown.Toggle>
+
+  <Dropdown.Menu className="drop-menu">
+    <Dropdown.Item href="#/action-1">Log Out</Dropdown.Item>
+  </Dropdown.Menu>
+</Dropdown>
+  </Container>
 </Navbar>
         </div>
     )
